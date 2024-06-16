68bf760c733d8585286e1cfa7f4f5ffd7b71efe7
Eduardo Neves Paschoal <enp396@gmail.com>
2024-06-16 16:25:27 -0300

    One solution to think about

--- parallelCodes/sum/problem.cpp
+++ parallelCodes/sum/problem.cpp
@@ -16,86 +16,27 @@ int MaxIncreasingSub(int arr[], int ArrayLength, int subArrayLength)
 	for (int i = 0; i < ArrayLength; i++) { 
 		dp[i][1] = arr[i]; //this initialize the first column of the matrix with the array elements
 	} 
-	/* //pragma 
-	for (int i = 1; i < ArrayLength; i++) { 
-		for (int j = 0; j < i; j++) { 
-			if (arr[j] < arr[i]) { 
-				for (int l = 1; l <= subArrayLength - 1; l++) { 
+	
+	for (int l = 1; l <= subArrayLength - 1; l++) {
+		#pragma omp parallel for
+		for (int i = 1; i < ArrayLength; i++) { 
+			for (int j = 0; j < i; j++) { 
+				if (arr[j] < arr[i]) { 
 					if (dp[j][l] != -1) { 
 						dp[i][l + 1] = max(dp[i][l + 1],dp[j][l] + arr[i]); 
 					} 
-				} 
+				}
 			} 
 		} 
-	} //this loop fills the matrix with the maximum sum of the increasing subsequence  */
-	
-	for (int row = 1; row <= subArrayLength - 1; row++){
-		# pragma omp paralell for 
-		for (int i = 1; i < ArrayLength; i++) { 
-			
-			int biggest = -1;
-			dp[i][row+1] = dp[i][row];
-
-			# pragma omp paralell for 
-			for (int j = 0; j < i; j++) { 
-				if (arr[j] < dp[i][row+1]) { 
-					biggest = max(arr[j], biggest);
-				} 	
-			} 
-			if(biggest != -1)
-				dp[i][row+1] = biggest;
-			else
-				dp[i][row+1] = -1;
-		}
 	}
+	 //this loop fills the matrix with the maximum sum of the increasing subsequence
 
-	int biggest_of_biggest = -1;
+	for (int i = 0; i < ArrayLength; i++) { 
+		if (ans < dp[i][subArrayLength]) 
+			ans = dp[i][subArrayLength]; 
+	} 
 
-	# pragma omp paralell for 
-	for(int i = 0; i < ArrayLength; i++){
-		
-		int row_sum = 0;
-		# pragma omp paralell for 
-		for(int j = 1; j <= subArrayLength -1; j++){
-			if(dp[i][j] != -1) row_sum +=  dp[i][j];
-			
-			// cout << dp[i][j] << " ";
-		}
-		// cout << endl << row_sum << " !!" << endl;
-		biggest_of_biggest = max(biggest_of_biggest, row_sum);
-	}
-
-
-	// for(int i = 0; i < ArrayLength; i++){
-	// 	for(int j = 0; j < subArrayLength; j++){
-	// 		cout << dp[i][j] << " "; //this initialize the matrix with -1
-	// 	}
-	// 	cout << endl;
-	// }
-
-
-	// cout << "AAA " << biggest_of_biggest << endl;
-// for (int l = 1; l <= subArrayLength - 1; l++) { 
-// 	#pragma omp paralell for
-// 	for (int i = 1; i < 60000; i++) { 
-// 		for (int j = 0; j < i; j++) { 
-// 			if (arr[j] < arr[i]) { 
-				
-// 					if (dp[j][l] != -1) { 
-// 						dp[i][l + 1] = max(dp[i][l + 1],dp[j][l] + arr[i]); 
-// 					} 
-				
-// 			} 
-// 		} 
-// 	} //this loop fills the matrix with the maximum sum of the increasing subsequence
-// } 
-
-	// for (int i = 0; i < ArrayLength; i++) { 
-	// 	if (ans < dp[i][subArrayLength]) 
-	// 		ans = dp[i][subArrayLength]; 
-	// } 
-
-	return (biggest_of_biggest == -1) ? 0 : biggest_of_biggest; 
+	return (ans == -1) ? 0 : ans; 
 } 
 
 int main() 
